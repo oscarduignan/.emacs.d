5043a37be2645b5a5d3a7e882fc10cce08cd1642
Oscar Duignan <oscarduignan@gmail.com>
2012-07-09 17:34:14 +0100

    updated todo list and my org-mode config to set some sensible defaults for the appearance

--- custom/setup-org-mode.el
+++ custom/setup-org-mode.el
@@ -1,4 +1,12 @@
-;; Normally this would be C-<RET>
-(define-key evil-normal-state-map (kbd "<RET>") 'org-insert-heading-respect-content)
+;; I don't like inconsistent tag allignment, make it so that tags are just
+;; immediately after headings
+(setq org-tags-column 0)
+
+;; Make my lists look good by default and have sensible line wrapping
+(add-hook 'org-mode-hook
+          (lambda ()
+            (org-indent-mode t)
+            (visual-line-mode t)
+          t))
 
 (provide 'setup-org-mode)
